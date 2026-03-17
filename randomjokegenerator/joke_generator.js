6c695ba7d4e5666c690e65444b4ea0d3d3910188
Nishant Bansal <nishant87706@gmail.com>
2026-03-17 10:44:30 +0530

    random joke cli generator project

---
+++ randomjokegenerator/joke_generator.js
@@ -0,0 +1,28 @@
+import https from "https";
+
+
+const getJoke=()=>{
+const url= `https://official-joke-api.appspot.com/random_joke`;
+
+https.get(url,(response)=>{
+    let data="";
+   response.on("data",(chunk)=>{
+    data +=chunk;
+   })
+
+   response.on("end",()=>{
+    const joke = JSON.parse(data);
+    console.log("Here is a random joke:");
+    console.log(joke.setup);
+    console.log(joke.punchline);
+
+   })
+
+   response.on("error",(err)=>{
+    console.log("Error: ",err.message);
+   })
+})
+ 
+}
+
+getJoke();
